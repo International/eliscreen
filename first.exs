e2f6e5184f2029ba059c32cec045175a619f9f47
George Opritescu <ssscripting@gmail.com>
2015-09-28 21:05:34 +0200

    Playing around with path functions

--- first.exs
+++ first.exs
@@ -1,18 +1,33 @@
-try do
-  for _ <- Stream.cycle([:ok]) do
+case System.argv do
+  [dir_name |_ ] ->
     {{year, month, day}, {hour, minute, sec}} = :calendar.local_time()
 
-    IO.puts year
-
-    case System.cmd "echo", ["hello"] do
-      {output, 0} ->
-        IO.puts "Output was #{output}"
-      {output, code} ->
-        IO.puts "Error, code #{code}"
+    path_start = Path.join(dir_name, to_string(year))
+    case File.mkdir_p(path_start) do
+      :ok ->
+        IO.puts "Succesfully created #{path_start}"
+      _ ->
+        IO.puts "Error!"
     end
+    #
+    # case System.cmd "echo", ["hello"] do
+  #   {output, 0} ->
+    #     IO.puts "Output was #{output}"
+  #   {output, code} ->
+    #     IO.puts "Error, code #{code}"
+    # end
 
-    throw :halt
-  end
-catch
-  :halt -> IO.puts "Finished processing"
+    #     throw :halt
+    #   end
+    # catch
+      #   :halt -> IO.puts "Finished processing"
+      # end
+
+   _ -> IO.puts "Please supply a folder name"
 end
+# if length(System.argv) == 1 do
+#   # try do
+#   #   for _ <- Stream.cycle([:ok]) do
+# else
+#   IO.puts "Need the folder name as an argument"
+# end
