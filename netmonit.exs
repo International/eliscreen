40eeaadfd867866308d0e67e0cf62a8512c61428
George Opritescu <ssscripting@gmail.com>
2016-05-07 13:23:54 +0300

    rewrote net check script

--- netmonit.exs
+++ netmonit.exs
@@ -1,27 +1,41 @@
 defmodule Netmonit do
   def check_net(state, _state_change_time) do
-    new_state = case System.cmd("ping",["-c", "1", "google.ro"]) do
-      {_output, code} when code != 0 -> 
-        if state != :down do
-          System.cmd "terminal-notifier",["-message", "#{format_time} No internet connection"]
-          :down
-        else
-          IO.puts "Internet still down"
-          state
-        end
+    new_state = case System.cmd("ping",["-c", "1", "google.com"]) do
+      {_output, code} when(code != 0) and (state != :down) -> 
+        :down
       _ ->
         if state != :up do
-          System.cmd "terminal-notifier",["-message", "#{format_time} Internet connection is back"]
           :up
         else
-          IO.puts "Internet still up"
           state
         end
     end
 
-    :timer.sleep(20000)
+    if new_state != state do
+      send_notification!(notification_msg(state, new_state))
+    else
+      IO.puts "Internet state is still: #{state}"
+    end
+
+    :timer.sleep(sleep_time)
     check_net(new_state, :calendar.local_time)
 
+  end
+
+  defp notification_msg(:up,:down) do
+    "No internet connection"
+  end
+
+  defp notification_msg(:down,:up) do
+    "Internet connection is back"
+  end
+
+  defp send_notification!(msg) do
+    System.cmd "terminal-notifier",["-message", "#{format_time} #{msg}"]
+  end
+
+  defp sleep_time do
+    5 * 60000
   end
 
   defp format_time do
